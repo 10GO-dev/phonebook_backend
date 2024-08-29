00a4b56e0f9846e565efc87422fbaaa20d9b699a
10GO-dev <Diegoperalta0929@gmail.com>
2024-08-29 19:58:03 -0400

    chore: update server port logging message

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.use(cors())
 app.use(express.json())
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
+    console.log(`Server running on  port ${PORT}`)
 })
 
 
