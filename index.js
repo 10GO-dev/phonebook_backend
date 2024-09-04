1002860cf5b1bfebf7b00558d884f3735af3cc82
10GO-dev <Diegoperalta0929@gmail.com>
2024-09-04 02:24:27 -0400

    uibuild

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 //get all persons
 app.get('/api/persons', (request, response) => {
 
-    Person.find({}).then(persons => {
-      response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 //find person
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
 
-    Person.findById(id)
+  Person.findById(id)
     .then(result => {
       response.json(result)
     })
@@ -38,63 +38,65 @@ app.get('/api/persons/:id', (request, response) => {
 
 //delete person
 app.delete('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id
-    Person.findByIdAndDelete(id)
-    .then( result => {
+  const id = request.params.id
+  Person.findByIdAndDelete(id)
+    // eslint-disable-next-line no-unused-vars
+    .then( _result => {
       response.status(204).end()
-    })
+    }).catch(error => next(error))
 })
 
 //add person
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   Person.findOne({ name: { $regex: `^${body.name}$`, $options: 'i' } })
-  .then(foundPerson => {
-    if(foundPerson) {
-      response.status(409).json({error: 'name must be unique'})
-    }
-    else {
-      const person = new Person({ name: body.name, number: String(body.number) })
-      person.save()
-      .then( savedPerson => {
-        response.json(savedPerson)
-      })
-      .catch(error => next(error))
-    }
-})
+    .then(foundPerson => {
+      if(foundPerson) {
+        response.status(409).json({ error: 'name must be unique' })
+      }
+      else {
+        const person = new Person({ name: body.name, number: String(body.number) })
+        person.save()
+          .then( savedPerson => {
+            response.json(savedPerson)
+          })
+          .catch(error => next(error))
+      }
+    })
 })
 
 
 //update person
 app.put('/api/persons/:id', (request, response,next) => {
-  body = request.body
+  const { name, number } = request.body
 
-  const person = {
-    name: body.name,
-    number: body.number
-  }
-
-  Person.findByIdAndUpdate(request.params.id, person, {new: true})
-  .then(updatedPerson => {
-    response.json(updatedPerson)
-  })
-  .catch(error => next(error))
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidator: true, context: 'query' })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 //info page
-app.get('/api/info', (request, response) => {
-    const date = new Date()
-    const info = `<p>Phonebook has info for ${persons.length} people</p>
+app.get('/api/info', (request, response,next) => {
+  const date = new Date()
+
+  Person.find({})
+    .then(persons => {
+      response.send(`<p>Phonebook has info for ${persons.length} people</p>
     <br>
     <p>${date.toString()}</p>
-    `
-    response.send(info)
+    `)
+    }).catch(error => next(error))
 })
 
 
 const unknownEndPoint = (request, response) => {
-  response.status(404).send({error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndPoint)
@@ -103,11 +105,11 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if(error.name === 'CastError'){
-    return response.status(400).send({ error: 'malformatted id'})
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
   if(error.name === 'ValidationError'){
-    return response.status(400).send({error: error.message})
+    return response.status(400).send({ error: error.message })
   }
 
   next(error)
@@ -117,5 +119,5 @@ app.use(errorHandler)
 
 
 app.listen(PORT, () => {
-    console.log(`Server running on  port ${PORT}`)
+  console.log(`Server running on  port ${PORT}`)
 })
